c760b7d56b086fd1f5af46b5dde3166d9f57b711
panurjoma <pablohl50@gmail.com>
2022-05-15 13:08:26 +0200

    Add new lessons to the course and exercises

---
+++ src/data_analysis/Exercise_House_Pricing.py
@@ -0,0 +1,14 @@
+#invite people for the Kaggle party
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+
+#bring in the six packs
+df_train = pd.read_csv('data/train.csv')
+
+print("test")
